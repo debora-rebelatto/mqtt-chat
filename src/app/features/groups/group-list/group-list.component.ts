e933736a6e9866e99e24f9c2d0c31996b8ce26c4
Débora Rebelatto <deborarebelatto1@gmail.com>
2025-10-05 16:42:39 -0300

    isLeader

--- src/app/features/groups/group-list/group-list.component.ts
+++ src/app/features/groups/group-list/group-list.component.ts
@@ -1,15 +1,14 @@
+import { AppStateService } from './../../../services/app-state.service'
 import { Component, Input, Output, EventEmitter } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { LucideAngularModule, Search } from 'lucide-angular'
-import { GroupChat } from '../../../models/group-chat.model'
-import { Group } from '../../../models/group.model'
-import { SelectedChat } from '../../../models/selected-chat.models'
 import { BadgeComponent } from '../../../components/badge/badge.component'
 import { ListContainerComponent } from '../../../components/list-container/list-container.component'
+import { Group, SelectedChat } from '../../../models'
 import { GroupCardComponent } from '../group-card/group-card.component'
 
 @Component({
-  selector: 'app-group-list',
+  selector: 'group-list',
   templateUrl: './group-list.component.html',
   standalone: true,
   imports: [
@@ -22,13 +21,18 @@ import { GroupCardComponent } from '../group-card/group-card.component'
 })
 export class GroupListComponent {
   readonly Search = Search
-  @Input() username: string = ''
 
   @Input() groups: Group[] = []
-  @Input() groupChat: GroupChat[] = []
+  @Input() groupChat: Group[] = []
   @Input() selectedChat: SelectedChat | null = null
 
   @Output() groupJoined = new EventEmitter<string>()
   @Output() createGroup = new EventEmitter<void>()
-  @Output() chatSelected = new EventEmitter<GroupChat>()
+  @Output() chatSelected = new EventEmitter<Group>()
+
+  constructor(private appService: AppStateService) {}
+
+  isLeader(leader: string): boolean {
+    return this.appService.username === leader ? true : false
+  }
 }
