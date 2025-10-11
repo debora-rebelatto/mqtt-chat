8888107cdf0a4d2e9a8396a142a79c5c31b21126
Débora Rebelatto <deborarebelatto1@gmail.com>
2025-10-11 17:32:31 -0300

    invitation now works without local storage

--- src/app/services/invitation.service.ts
+++ src/app/services/invitation.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core'
 import { BehaviorSubject } from 'rxjs'
 import { MqttService } from './mqtt.service'
 import { GroupInvitation } from '../models/group-invitation.model'
-import { User } from '../models'
+import { Group, User } from '../models'
 import { MqttTopics } from '../config/mqtt-topics'
 import { AppStateService } from './app-state.service'
 import { IdGeneratorService } from './id-generator.service'
@@ -34,33 +34,35 @@ export class InvitationService {
     })
   }
 
-  requestJoinGroup(groupId: string, groupName: string, requester: User, leader: User) {
-    const requestKey = `${requester.id}_${groupId}`
+  requestJoinGroup(group: Group) {
+    const requesterId = this.currentUser.id
+    const requestKey = `${requesterId}_${group.id}`
 
     if (this.pendingRequests.has(requestKey)) {
       return false
     }
 
+    if (!group.leader || !group.leader.id) {
+      return false
+    }
+
     const invitationId = this.idGenerator.generateRequestId()
-    const joinRequest = new GroupInvitation(invitationId, groupId, groupName, requester, new Date())
+    const joinRequest = new GroupInvitation(
+      invitationId,
+      group.id,
+      group.name,
+      requesterId,
+      new Date()
+    )
 
     const payload = {
       ...joinRequest,
-      invitee: {
-        id: requester.id,
-        name: requester.name,
-        online: requester.online || true,
-        lastSeen: requester.lastSeen || new Date()
-      },
-      leader: {
-        id: leader.id,
-        name: leader.name
-      },
+      invitee: requesterId,
       type: 'join_request'
     }
 
     this.pendingRequests.add(requestKey)
-    this.mqttService.publish(MqttTopics.sendInvitation(leader.id), JSON.stringify(payload))
+    this.mqttService.publish(MqttTopics.sendInvitation(group.leader.id), JSON.stringify(payload))
 
     return true
   }
@@ -69,12 +71,7 @@ export class InvitationService {
     const response = {
       invitationId: invitation.id,
       groupId: invitation.groupId,
-      invitee: {
-        id: invitation.invitee.id,
-        name: invitation.invitee.name,
-        online: invitation.invitee.online,
-        lastSeen: invitation.invitee.lastSeen
-      },
+      invitee: invitation.invitee,
       accepted: true,
       timestamp: new Date()
     }
@@ -82,7 +79,7 @@ export class InvitationService {
     this.mqttService.publish(MqttTopics.invitationResponses, JSON.stringify(response))
     this.removeInvitation(invitation.id)
 
-    const requestKey = `${invitation.invitee.id}_${invitation.groupId}`
+    const requestKey = `${invitation.invitee}_${invitation.groupId}`
     this.pendingRequests.delete(requestKey)
   }
 
@@ -98,19 +95,14 @@ export class InvitationService {
     this.mqttService.publish(MqttTopics.invitationResponses, JSON.stringify(response))
     this.removeInvitation(invitation.id)
 
-    const requestKey = `${invitation.invitee.id}_${invitation.groupId}`
+    const requestKey = `${invitation.invitee}_${invitation.groupId}`
     this.pendingRequests.delete(requestKey)
   }
 
   private handleInvitation(message: string) {
     const data = JSON.parse(message)
 
-    const invitee = new User(
-      data.invitee.id,
-      data.invitee.name,
-      data.invitee.online,
-      new Date(data.invitee.lastSeen)
-    )
+    const invitee = data.invitee
 
     const invitation = new GroupInvitation(
       data.id,
@@ -132,16 +124,6 @@ export class InvitationService {
   private removeInvitation(invitationId: string) {
     const invitations = this.invitationsSubject.value.filter((i) => i.id !== invitationId)
     this.invitationsSubject.next(invitations)
-  }
-
-  clearInvitations() {
-    this.invitationsSubject.next([])
-    this.pendingRequests.clear()
-  }
-
-  hasPendingRequest(groupId: string): boolean {
-    const requestKey = `${this.currentUser.id}_${groupId}`
-    return this.pendingRequests.has(requestKey)
   }
 
   onDisconnect() {
