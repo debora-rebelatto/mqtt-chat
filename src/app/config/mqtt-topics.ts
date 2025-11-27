fb837000d816eb742063a691b2b2161a0d8a6c90
Bruno de Macedo Sanagiotto <62435631+brunodms@users.noreply.github.com>
2025-11-27 19:37:22 -0300

    implementado ID_Control e removidas funções anteriores a implementação ou atualizadas com a nova lógica (#83)
    * resolvido 👍
    
    * alguns ajustes para começar a funcionar com ID_Control, removidos users padrão(pq tava causando alguns bugs nos chats) e alterei o retained para false, pois estava aparecendo mensagens de outros usuarios ao criar uma conta nova
    
    * removidas funções anteriores a implementação do ID_Control e atualizadas com a nova lógica, ajuste no publish removendo redundancia

--- src/app/config/mqtt-topics.ts
+++ src/app/config/mqtt-topics.ts
@@ -1,7 +1,7 @@
 const BASE_TOPIC = 'meu-chat-mqtt'
 
 export const MqttTopics = {
-  privateMessage: (username: string) => `${BASE_TOPIC}/messages/${username}`,
+  control: (userId: string) => `${userId}_Control`,
 
   groupMessages: `${BASE_TOPIC}/messages/groups`,
   specificGroup: (groupId: string) => `${BASE_TOPIC}/groups/${groupId}`,
@@ -21,10 +21,5 @@ export const MqttTopics = {
   sendInvitation: (username: string) => `${BASE_TOPIC}/invitations/${username}`,
   invitationResponses: `${BASE_TOPIC}/invitations/responses`,
   groupUpdates: `${BASE_TOPIC}/group-updates`,
-  privateChat: {
-    request: (username: string) => `${BASE_TOPIC}/private-chat/request/${username}`,
-    response: (username: string) => `${BASE_TOPIC}/private-chat/response/${username}`,
-    allowed: (username: string) => `${BASE_TOPIC}/private-chat/allowed/${username}`
-  },
   authRegistry: 'chat/users/registry'
 }
